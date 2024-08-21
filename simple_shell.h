e472f5f3972572e2bf27c30455dd7b0ddec5274d
MauriceNgicho <mrcngicho@gmail.com>
2024-08-21 02:11:11 -0700

    header files

---
+++ simple_shell.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif /* SIMPLE_SHELL */
